0af07d1de780f1a03d7089fd64da0b818b262ebb
LSH-21 <1967925767@qq.com>
2021-12-01 20:59:07 +0800

    2021.12.1

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,7 +6,7 @@ export default {
             //2.判断oldProduct
             if (oldProduct) {
                 context.commit('addCounter', oldProduct)
-                resolve('当前商品数量 +1')
+                resolve('当前商品数量 +1');
             } else {
                 payload.count = 1;
                 // context.state.cartList.push(payload)
